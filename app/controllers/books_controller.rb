ca98a7a5e732a008ac948a77e88c385d0c6ae3d5
sr1011 <ryo.s.1011.football@gmail.com>
2021-10-03 06:27:06 +0000

    Second Commit

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,9 @@
 class BooksController < ApplicationController
-
-  def show
-    @book = Book.find(params[:id])
+  
+  def new
+    @book = Book.new
   end
-
-  def index
-    @books = Book.all
-  end
-
+  
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
@@ -19,11 +15,26 @@ class BooksController < ApplicationController
     end
   end
 
-  def edit
+  def show
     @book = Book.find(params[:id])
+    @user = @book.user
+    @new_book = Book.new
   end
 
+  def index
+    @user = current_user
+    @book = Book.new
+    @books = Book.all
+  end
 
+  def edit
+    @book = Book.find(params[:id])
+    if @book.user == current_user
+      render "edit"
+    else
+      redirect_to books_path
+    end
+  end
 
   def update
     @book = Book.find(params[:id])
@@ -34,16 +45,19 @@ class BooksController < ApplicationController
     end
   end
 
-  def delete
-    @book = Book.find(params[:id])
-    @book.destoy
+  def destroy
+    book = Book.find(params[:id])
+    if book.destroy
+    flash[:notice] = "successfully"
     redirect_to books_path
+    else
+      render book_path(current_user)
+    end
   end
 
   private
 
   def book_params
-    params.require(:book).permit(:title)
+    params.require(:book).permit(:title, :body)
   end
-
 end
