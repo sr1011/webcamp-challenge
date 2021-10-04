47e368c48034fd825221b96a90b957cc5ae40cdc
sr1011 <ryo.s.1011.football@gmail.com>
2021-10-04 10:22:37 +0000

    Commit4

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about', as: 'about'
   devise_for :users
-  resources :users,only: [:show,:index,:edit,:update]
+  resources :users,only: [:show,:index,:edit,:update] do
+    resource :relationships, only: [:create, :destroy]
+    get 'followings' => 'relationships#followings', as: 'followings'
+    get 'followers' => 'relationships#followers', as: 'followers'
+  end
   resources :books do
       resource :favorites, only: [:create, :destroy]
        resources :post_comments, only: [:create, :destroy]
