970700b2fa63b7ad44b05f4732f7ef1abbaa990b
sr1011 <ryo.s.1011.football@gmail.com>
2021-10-09 11:19:06 +0000

    Commit6

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,10 @@
 class BooksController < ApplicationController
   before_action :authenticate_user!
-  
+
   def new
     @book = Book.new
   end
-  
+
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -1,22 +1,23 @@
 class PostCommentsController < ApplicationController
-  
+
   def create
-    book = Book.find(params[:book_id])
-    comment = current_user.post_comments.new(post_comment_params)
-    comment.book_id = book.id
-    comment.save
-    redirect_to book_path(book)
+    @book = Book.find(params[:book_id])
+    @comment = current_user.post_comments.new(post_comment_params)
+    @comment.book_id = @book.id
+    @comment.save
+    @post_comment = PostComment.new
   end
 
   def destroy
-    PostComment.find_by(id: params[:id]).destroy
-    redirect_to book_path(params[:book_id])
+    @book = Book.find(params[:book_id])
+  	@post_comment = @book.post_comments.find(params[:id])
+		@post_comment.destroy
   end
-  
+
   private
 
   def post_comment_params
     params.require(:post_comment).permit(:comment)
   end
-  
+
 end
